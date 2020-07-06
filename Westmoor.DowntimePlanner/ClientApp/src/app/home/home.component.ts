bd3a735df21ef68e9725538dc91283c456a74a3e
Etienne Maheu <contact@etiennemaheu.com>
2020-07-06 19:54:15 -0400

    Add jexl to the front-end

--- Westmoor.DowntimePlanner/ClientApp/src/app/home/home.component.ts
+++ Westmoor.DowntimePlanner/ClientApp/src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import * as jexl from 'jexl';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,11 @@ import { AuthService } from '../auth.service';
 })
 export class HomeComponent {
   user$ = this.auth.user$;
+
+  jexlResult$ = of('1+1')
+    .pipe(
+      switchMap(exp => jexl.eval(exp))
+    );
 
   constructor(
     private auth: AuthService
