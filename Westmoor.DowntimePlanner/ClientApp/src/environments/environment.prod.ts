c5bc1706f4023258b265191e207e4fd97887bc05
Etienne Maheu <contact@etiennemaheu.com>
2020-07-18 23:57:22 -0400

    Add production config for oidc in the client

--- Westmoor.DowntimePlanner/ClientApp/src/environments/environment.prod.ts
+++ Westmoor.DowntimePlanner/ClientApp/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  oidc: {
+    authority: 'dev-kawazoe.us.auth0.com',
+    client_id: 'VxaXJsnUSCHHjKVwN7jFemYVXkKP3F59',
+    redirect_url: '/oidc-callback',
+    audience: 'westmoor-downtime-planner.azurewebsites.net'
+  }
 };
