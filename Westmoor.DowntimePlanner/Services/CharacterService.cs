73e8871aeaa69cca41764c4bee3d03b911490379
Etienne Maheu <contact@etiennemaheu.com>
2020-07-30 12:34:29 -0400

    Share downtimes automatically when a character is shared

--- Westmoor.DowntimePlanner/Services/CharacterService.cs
+++ Westmoor.DowntimePlanner/Services/CharacterService.cs
@@ -10,10 +10,15 @@ namespace Westmoor.DowntimePlanner.Services
     public class CharacterService : ICharacterService
     {
         private readonly ICharacterRepository _repository;
+        private readonly IDowntimeRepository _downtimeRepository;
 
-        public CharacterService(ICharacterRepository repository)
+        public CharacterService(
+            ICharacterRepository repository,
+            IDowntimeRepository downtimeRepository
+        )
         {
             _repository = repository;
+            _downtimeRepository = downtimeRepository;
         }
 
         public async Task<CharacterResponse[]> GetAllAsync() =>
@@ -27,8 +32,31 @@ namespace Westmoor.DowntimePlanner.Services
         public async Task CreateAsync(CreateCharacterRequest request) =>
             await _repository.CreateAsync(request);
 
-        public async Task UpdateAsync(string id, UpdateCharacterRequest request) =>
+        public async Task UpdateAsync(string id, UpdateCharacterRequest request)
+        {
             await _repository.UpdateAsync(id, request);
+
+            var ownedDowntimes = (await _downtimeRepository.GetAllAsync())
+                .Where(d => d.Character.Id == id);
+
+            foreach (var downtime in ownedDowntimes)
+            {
+                var downtimeUpdate = new UpdateDowntimeRequest
+                {
+                    Costs = downtime.Costs
+                        .Select(c => new UpdateDowntimeCostRequest
+                        {
+                            ActivityCostKind = c.ActivityCostKind,
+                            Goal = c.Goal,
+                            Value = c.Value
+                        })
+                        .ToArray(),
+                    SharedWith = request.SharedWith
+                };
+
+                await _downtimeRepository.UpdateAsync(downtime.Id, downtimeUpdate);
+            }
+        }
 
         public async Task DeleteAsync(string id) =>
             await _repository.DeleteAsync(id);
