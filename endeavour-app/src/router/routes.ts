09ffab31a467ad6d7cc96d25ebc465083d6fee92
Etienne Maheu <contact@etiennemaheu.com>
2021-09-08 20:52:52 -0400

    Enable noUncheckedIndexedAccess

--- endeavour-app/src/router/routes.ts
+++ endeavour-app/src/router/routes.ts
@@ -2,12 +2,14 @@ import { computed, inject } from 'vue';
 import { useRoute, viewDepthKey } from 'vue-router';
 import type { ComputedRef } from 'vue';
 
+import { _throw } from '@/lib/functional';
+
 export function useRelativeRoute(): ComputedRef<string> {
   const route = useRoute();
   const depth = inject(viewDepthKey, route.matched.length);
 
   return computed(() => {
-    const path = route.matched[depth - 1].path;
+    const path = route.matched[depth - 1]?.path ?? _throw(new Error(`Could not find da valid path for route "${route.path}" at depth "${depth - 1}"`));
     return Object.entries(route.params)
       .sort((([aKey], [bKey]) => (aKey.length > bKey.length ? 1 : aKey.length < bKey.length ? -1 : 0)))
       .reduce((acc, [key, param]) => acc.replace(`:${key}`, param as string), path);
