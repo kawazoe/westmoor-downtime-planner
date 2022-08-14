c290053d0f24e121d68e304913760de4cbe4690a
Etienne Maheu <contact@etiennemaheu.com>
2022-08-13 21:18:09 -0400

    Update dependencies
    Fix type inferences issues in setup style stores

--- endeavour-app/src/router/routes.ts
+++ endeavour-app/src/router/routes.ts
@@ -1,17 +1,26 @@
 import { computed, inject } from 'vue';
-import { useRoute, viewDepthKey } from 'vue-router';
 import type { ComputedRef } from 'vue';
+
+import { matchedRouteKey, useRoute } from 'vue-router';
 
 import { _throw } from '@/lib/_throw';
 
+function longestToShortest(aKey: string, bKey: string) {
+  return aKey.length > bKey.length
+    ? 1
+    : aKey.length < bKey.length
+      ? -1
+      : 0;
+}
+
 export function useRelativeRoute(): ComputedRef<string> {
   const route = useRoute();
-  const depth = inject(viewDepthKey, route.matched.length);
+  const closestRoute = inject(matchedRouteKey);
 
   return computed(() => {
-    const path = route.matched[depth - 1]?.path ?? _throw(new Error(`Could not find da valid path for route "${route.path}" at depth "${depth - 1}"`));
+    const path = closestRoute?.value?.path ?? _throw(new Error('[useRelativeRoute] could not find the closest RouterView.'));
     return Object.entries(route.params)
-      .sort((([aKey], [bKey]) => (aKey.length > bKey.length ? 1 : aKey.length < bKey.length ? -1 : 0)))
+      .sort(([aKey], [bKey]) => longestToShortest(aKey, bKey))
       .reduce((acc, [key, param]) => acc.replace(`:${key}`, param as string), path);
   });
 }
