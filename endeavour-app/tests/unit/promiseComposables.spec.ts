c3b0478cafd53eec186a20d04e355fd0209f0bb5
Etienne Maheu <contact@etiennemaheu.com>
2022-08-29 21:02:39 -0400

    Remove pinia's dependencies in promise tests

--- endeavour-app/tests/unit/promiseComposables.spec.ts
+++ endeavour-app/tests/unit/promiseComposables.spec.ts
@@ -1,124 +1,122 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest';
-import { createPinia, setActivePinia } from 'pinia';
 
 import { _throw } from '@/lib/_throw';
 import { stall } from '../lib/mockPromise';
 
-import { definePromiseStore } from '@/stores/promiseStore';
+import { usePromise } from '@/composables/promises';
 
 const mockKeySelector = vi.fn(() => 'test');
 const mockEmptyPredicate = vi.fn(v => v === 'success');
 
-describe('Promise Store', () => {
+describe('Promise Composable', () => {
   beforeEach(() => {
-    setActivePinia(createPinia());
     vi.restoreAllMocks();
   });
 
   describe('State machine (base states)', () => {
     it('should be created in a neutral state', () => {
       const trigger = vi.fn(async () => 'success');
-      const sut = definePromiseStore('test', trigger)();
+      const sut = usePromise(trigger);
 
       expect(trigger).not.toHaveBeenCalled();
 
-      expect(sut.status).toBe('initial');
-      expect(sut.state.cacheKey).toBe('');
-      expect(sut.value).toBeUndefined();
-      expect(sut.error).toBeUndefined();
+      expect(sut.status.value).toBe('initial');
+      expect(sut.state.value.cacheKey).toBe('');
+      expect(sut.value.value).toBeUndefined();
+      expect(sut.error.value).toBeUndefined();
     });
 
     it('should enter a loading state while waiting for the trigger', () => {
       const trigger = vi.fn(() => stall());
-      const sut = definePromiseStore('test', trigger)();
+      const sut = usePromise(trigger);
 
       sut.trigger();
 
       expect(trigger).toHaveBeenCalledOnce();
 
-      expect(sut.status).toBe('loading');
-      expect(sut.value).toBeUndefined();
-      expect(sut.error).toBeUndefined();
+      expect(sut.status.value).toBe('loading');
+      expect(sut.value.value).toBeUndefined();
+      expect(sut.error.value).toBeUndefined();
     });
 
     it('should enter a content state when the trigger returns a value', async () => {
       const trigger = vi.fn(async () => 'success');
-      const sut = definePromiseStore('test', trigger)();
+      const sut = usePromise(trigger);
 
       await sut.trigger();
 
       expect(trigger).toHaveBeenCalledOnce();
 
-      expect(sut.status).toBe('content');
-      expect(sut.value).toBe('success');
-      expect(sut.error).toBeUndefined();
+      expect(sut.status.value).toBe('content');
+      expect(sut.value.value).toBe('success');
+      expect(sut.error.value).toBeUndefined();
     });
 
     describe('default empty predicate', () => {
       for (const emptyValue of [null, '', []]) {
         it(`should enter an empty state when the trigger returns "${JSON.stringify(emptyValue)}"`, async () => {
           const trigger = vi.fn(async () => emptyValue);
-          const sut = definePromiseStore('test', trigger)();
+          const sut = usePromise(trigger);
 
           await sut.trigger();
 
-          expect(sut.status).toBe('empty');
-          expect(sut.value).toStrictEqual(emptyValue);
-          expect(sut.error).toBeUndefined();
+          expect(sut.status.value).toBe('empty');
+          expect(sut.value.value).toStrictEqual(emptyValue);
+          expect(sut.error.value).toBeUndefined();
         });
       }
     });
 
     it('should enter an empty state when the content matches the empty predicate', async () => {
       const trigger = vi.fn(async () => 'success');
-      const sut = definePromiseStore('test', trigger, { emptyPredicate: mockEmptyPredicate })();
+      const sut = usePromise(trigger, { emptyPredicate: mockEmptyPredicate });
 
       await sut.trigger();
 
       expect(mockEmptyPredicate).toHaveBeenCalledOnce();
 
-      expect(sut.status).toBe('empty');
-      expect(sut.value).toBe('success');
-      expect(sut.error).toBeUndefined();
+      expect(sut.status.value).toBe('empty');
+      expect(sut.value.value).toBe('success');
+      expect(sut.error.value).toBeUndefined();
     });
 
     it('should enter an error state when the trigger throws', async () => {
       const trigger = vi.fn(async () => _throw('failure'));
-      const sut = definePromiseStore('test', trigger)();
+      const sut = usePromise(trigger);
 
       await sut.trigger();
 
       expect(trigger).toHaveBeenCalledOnce();
 
-      expect(sut.status).toBe('error');
-      expect(sut.value).toBeUndefined();
-      expect(sut.error).toBe('failure');
+      expect(sut.status.value).toBe('error');
+      expect(sut.value.value).toBeUndefined();
+      expect(sut.error.value).toBe('failure');
     });
   });
 
   describe('State machine (advanced states)', () => {
     it('should enter a refreshing state while waiting for a re-trigger from content', async () => {
       const trigger = vi.fn(() => stall());
-      const sut = definePromiseStore('test', trigger, { keySelector: mockKeySelector })();
+      const sut = usePromise(trigger, { keySelector: mockKeySelector });
 
       const call = sut.trigger();
       trigger.mock.results[0]?.value.resolve('success');
       await call;
 
-      expect(sut.status).toBe('content');
+      expect(sut.status.value).toBe('content');
 
       // noinspection ES6MissingAwait
       sut.trigger();
 
       expect(trigger).toHaveBeenCalledTimes(2);
-      expect(sut.status).toBe('refreshing');
-      expect(sut.value).toBe('success');
-      expect(sut.error).toBeUndefined();
+      expect(sut.status.value).toBe('refreshing');
+      expect(sut.value.value).toBe('success');
+      expect(sut.error.value).toBeUndefined();
     });
 
     it('should enter a loading state while waiting for a re-trigger from empty', async () => {
       const trigger = vi.fn(() => stall());
-      const sut = definePromiseStore('test', trigger, { keySelector: mockKeySelector })();
+      const sut = usePromise(trigger, { keySelector: mockKeySelector });
 
       const call = sut.trigger();
       trigger.mock.results[0]?.value.resolve(null);
@@ -129,14 +127,14 @@ describe('Promise Store', () => {
 
       expect(trigger).toHaveBeenCalledTimes(2);
 
-      expect(sut.status).toBe('loading');
-      expect(sut.value).toBeUndefined();
-      expect(sut.error).toBeUndefined();
+      expect(sut.status.value).toBe('loading');
+      expect(sut.value.value).toBeUndefined();
+      expect(sut.error.value).toBeUndefined();
     });
 
     it('should enter a retrying state while waiting for a re-trigger from error', async () => {
       const trigger = vi.fn(() => stall());
-      const sut = definePromiseStore('test', trigger, { keySelector: mockKeySelector })();
+      const sut = usePromise(trigger, { keySelector: mockKeySelector });
 
       const call = sut.trigger();
       trigger.mock.results[0]?.value.reject('failure');
@@ -147,80 +145,80 @@ describe('Promise Store', () => {
 
       expect(trigger).toHaveBeenCalledTimes(2);
 
-      expect(sut.status).toBe('retrying');
-      expect(sut.value).toBeUndefined();
-      expect(sut.error).toBe('failure');
+      expect(sut.status.value).toBe('retrying');
+      expect(sut.value.value).toBeUndefined();
+      expect(sut.error.value).toBe('failure');
     });
   });
 
   describe('Caching', () => {
     it('should set a cache key when the trigger is called', () => {
       const trigger = vi.fn(() => stall());
-      const sut = definePromiseStore('test', trigger)();
+      const sut = usePromise(trigger);
 
-      const previousKey = sut.state.cacheKey;
+      const previousKey = sut.state.value.cacheKey;
       sut.trigger();
 
-      expect(sut.state.cacheKey).not.toBe(previousKey);
+      expect(sut.state.value.cacheKey).not.toBe(previousKey);
     });
 
     it('should not change the cache key on resolve', async () => {
       const trigger = vi.fn(async () => 'success');
-      const sut = definePromiseStore('test', trigger, { keySelector: mockKeySelector })();
+      const sut = usePromise(trigger, { keySelector: mockKeySelector });
 
       const call = sut.trigger();
-      const previousKey = sut.state.cacheKey;
+      const previousKey = sut.state.value.cacheKey;
       await call;
 
       expect(mockKeySelector).toHaveBeenCalledOnce();
-      expect(sut.state.cacheKey).toBe(previousKey);
+      expect(sut.state.value.cacheKey).toBe(previousKey);
     });
 
     it('should not change the cache key on reject', async () => {
       const trigger = vi.fn(async () => _throw('failure'));
-      const sut = definePromiseStore('test', trigger, { keySelector: mockKeySelector })();
+      const sut = usePromise(trigger, { keySelector: mockKeySelector });
 
       const call = sut.trigger();
-      const previousKey = sut.state.cacheKey;
+      const previousKey = sut.state.value.cacheKey;
       await call;
 
       expect(mockKeySelector).toHaveBeenCalledOnce();
-      expect(sut.state.cacheKey).toBe(previousKey);
+      expect(sut.state.value.cacheKey).toBe(previousKey);
     });
 
     it('should clear cache on re-trigger when key changes', async () => {
       const trigger = vi.fn(async () => 'success');
-      const sut = definePromiseStore('test', trigger)();
+      const sut = usePromise(trigger);
 
       await sut.trigger();
-      const previousKey = sut.state.cacheKey;
+      const previousKey = sut.state.value.cacheKey;
       // noinspection ES6MissingAwait
       sut.trigger();
 
-      expect(sut.state.cacheKey).not.toBe(previousKey);
-      expect(sut.status).toBe('loading');
-      expect(sut.value).toBeUndefined();
-      expect(sut.error).toBeUndefined();
+      expect(sut.state.value.cacheKey).not.toBe(previousKey);
+      expect(sut.status.value).toBe('loading');
+      expect(sut.value.value).toBeUndefined();
+      expect(sut.error.value).toBeUndefined();
     });
 
     it('should use first param as cache key by default', async () => {
       const trigger = vi.fn(async (arg1: string) => arg1);
-      const sut = definePromiseStore('test', trigger)();
+      const sut = usePromise(trigger);
 
       await sut.trigger('key');
-      const previousKey = sut.state.cacheKey;
+      const previousKey = sut.state.value.cacheKey;
       // noinspection ES6MissingAwait
       sut.trigger('key');
 
-      expect(sut.state.cacheKey).toBe(previousKey);
-      expect(sut.state.cacheKey).toBe('key');
+      expect(sut.state.value.cacheKey).toBe(previousKey);
+      expect(sut.state.value.cacheKey).toBe('key');
     });
   });
 
   describe('Safeties', () => {
     it('should not re-trigger while loading for the same key', async () => {
       const trigger = vi.fn(() => stall());
-      const sut = definePromiseStore('test', trigger, { keySelector: mockKeySelector })();
+      const sut = usePromise(trigger, { keySelector: mockKeySelector });
 
       // noinspection ES6MissingAwait
       sut.trigger();
@@ -229,12 +227,12 @@ describe('Promise Store', () => {
 
       expect(trigger).toHaveBeenCalledOnce();
 
-      expect(sut.status).toBe('loading');
+      expect(sut.status.value).toBe('loading');
     });
 
     it('should not re-trigger while refreshing for the same key', async () => {
       const trigger = vi.fn(() => stall());
-      const sut = definePromiseStore('test', trigger, { keySelector: mockKeySelector })();
+      const sut = usePromise(trigger, { keySelector: mockKeySelector });
 
       const call = sut.trigger();
       trigger.mock.results[0]?.value.resolve('success');
@@ -248,12 +246,12 @@ describe('Promise Store', () => {
 
       expect(trigger).toHaveBeenCalledOnce();
 
-      expect(sut.status).toBe('refreshing');
+      expect(sut.status.value).toBe('refreshing');
     });
 
     it('should not re-trigger while retrying for the same key', async () => {
       const trigger = vi.fn(() => stall());
-      const sut = definePromiseStore('test', trigger, { keySelector: mockKeySelector })();
+      const sut = usePromise(trigger, { keySelector: mockKeySelector });
 
       const call = sut.trigger();
       trigger.mock.results[0]?.value.reject('failure');
@@ -267,13 +265,13 @@ describe('Promise Store', () => {
 
       expect(trigger).toHaveBeenCalledOnce();
 
-      expect(sut.status).toBe('retrying');
+      expect(sut.status.value).toBe('retrying');
     });
 
     it('should not update when trigger resolves out of turn', async () => {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const trigger = vi.fn((_: string) => stall());
-      const sut = definePromiseStore('test', trigger)();
+      const sut = usePromise(trigger);
 
       const call1 = sut.trigger('key1');
       const call2 = sut.trigger('key2');
@@ -284,15 +282,15 @@ describe('Promise Store', () => {
       await call1;
       await call2;
 
-      expect(sut.status).toBe('content');
-      expect(sut.state.cacheKey).toBe('key2');
-      expect(sut.value).toBe('success2');
+      expect(sut.status.value).toBe('content');
+      expect(sut.state.value.cacheKey).toBe('key2');
+      expect(sut.value.value).toBe('success2');
     });
 
     it('should not update when trigger rejects out of turn', async () => {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const trigger = vi.fn((_: string) => stall());
-      const sut = definePromiseStore('test', trigger)();
+      const sut = usePromise(trigger);
 
       const call1 = sut.trigger('key1');
       const call2 = sut.trigger('key2');
@@ -303,9 +301,9 @@ describe('Promise Store', () => {
       await call1;
       await call2;
 
-      expect(sut.status).toBe('error');
-      expect(sut.state.cacheKey).toBe('key2');
-      expect(sut.error).toBe('failure2');
+      expect(sut.status.value).toBe('error');
+      expect(sut.state.value.cacheKey).toBe('key2');
+      expect(sut.error.value).toBe('failure2');
     });
   });
 });
